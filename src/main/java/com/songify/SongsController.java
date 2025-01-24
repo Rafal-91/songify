6188273d92146ab1ff3c85fcfa2ace0569dd5dc0
Rafał Marchocki <marchocki.1991@gmail.com>
2025-01-24 10:05:37 +0100

    fetching data with using RequestParam

--- src/main/java/com/songify/SongsController.java
+++ src/main/java/com/songify/SongsController.java
@@ -4,20 +4,33 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class SongsController {
 
     Map<Integer, String> database = new HashMap<>();
 
-
     @GetMapping("/songs")
-    public ResponseEntity<SongsResponseDto> getAllSongs(){
+    public ResponseEntity<SongsResponseDto> getAllSongs(@RequestParam(required = false) Integer limit){
         database.put(1, "shawnmendes song1");
         database.put(2, "ariana grande song2");
+        database.put(3, "ariana grande asdfadsfadsf");
+        database.put(4, "ariana grande sdfadsfdsfds");
+        database.put(5, "ariana grande sosdfdsfdsfng2");
+
+        if (limit != null) {
+            Map<Integer, String> limitedMap = database.entrySet()
+                    .stream()
+                    .limit(limit)
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+            SongsResponseDto response = new SongsResponseDto(limitedMap);
+            return ResponseEntity.ok(response);
+        }
         SongsResponseDto response = new SongsResponseDto(database);
         return ResponseEntity.ok(response);
     }
