73ba9e6cee16f648f7bc0cb1fbed30601d8f6873
Rafał Marchocki <marchocki.1991@gmail.com>
2025-07-17 20:08:10 +0200

    added album, artist and genre

---
+++ src/main/java/com/songify/domain/crud/album/Album.java
@@ -0,0 +1,29 @@
+package com.songify.domain.crud.album;
+
+import com.songify.domain.crud.util.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+class Album extends BaseEntity {
+
+    @Id
+    @GeneratedValue(generator = "album_id_seq", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(
+            name = "album_id_seq",
+            sequenceName = "album_id_seq",
+            allocationSize = 1
+    )
+    private Long id;
+
+    private String title;
+
+    private Instant releaseDate;
+}
