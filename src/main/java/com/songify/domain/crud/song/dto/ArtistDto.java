224c3ecf8db32e6084e0fcd5372284e9c5c9822d
Rafał Marchocki <marchocki.1991@gmail.com>
2025-08-02 19:00:35 +0200

    adding artist saving feature

---
+++ src/main/java/com/songify/domain/crud/song/dto/ArtistDto.java
@@ -0,0 +1,6 @@
+package com.songify.domain.crud.song.dto;
+
+public record ArtistDto(Long id, String name) {
+
+}
+
---
+++ src/main/java/com/songify/infrastructure/crud/song/controller/ArtistRestController.java
@@ -0,0 +1,24 @@
+package com.songify.infrastructure.crud.song.controller;
+
+import com.songify.domain.crud.song.SongifyCrudFacade;
+import com.songify.domain.crud.song.dto.ArtistDto;
+import com.songify.domain.crud.song.dto.ArtistRequestDto;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/artist")
+class ArtistRestController {
+    private final SongifyCrudFacade  songifyCrudFacade;
+
+    @PostMapping
+    ResponseEntity<ArtistDto> postArtist(@RequestBody ArtistRequestDto artistRequestDto) {
+        ArtistDto artistDto = songifyCrudFacade.addArtist(artistRequestDto);
+        return ResponseEntity.ok(artistDto);
+    }
+}
