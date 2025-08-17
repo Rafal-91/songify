e768923e95489b8af17bf7f1138465e64a9b94f4
Rafał Marchocki <marchocki.1991@gmail.com>
2025-08-17 21:13:25 +0200

    added correct names for tests

--- src/test/java/com/songify/domain/crud/SongifyCrudFacadeTest.java
+++ src/test/java/com/songify/domain/crud/SongifyCrudFacadeTest.java
@@ -2,6 +2,7 @@ package com.songify.domain.crud;
 
 import com.songify.domain.crud.dto.ArtistDto;
 import com.songify.domain.crud.dto.ArtistRequestDto;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,23 +17,29 @@ class SongifyCrudFacadeTest {
     );
 
     @Test
-    public void first() {
-        ArtistRequestDto artistRequestDto = ArtistRequestDto.builder()
+    @DisplayName("Should add artist 'amigo' with id:0 When amigo was sent")
+    public void should_add_artist_amigo_with_id_zero_when_amigo_was_sent() {
+        // given
+        ArtistRequestDto shawnMendes = ArtistRequestDto.builder()
                 .name("amigo")
                 .build();
-        ArtistDto response = songifyCrudFacade.addArtist(artistRequestDto);
-
-        assertThat(response.id()).isEqualTo(5L);
-//        assert response.id().equals(5L);
+        // when
+        ArtistDto response = songifyCrudFacade.addArtist(shawnMendes);
+        // then
+        assertThat(response.id()).isEqualTo(0L);
+        assertThat(response.name()).isEqualTo("amigo");
     }
 
     @Test
-    public void first_wariant_B() {
+    @DisplayName("Should add artist 'shawn mendes' with id:0 When shawn mendes was sent")
+    public void should_add_artist_shawn_mendes_with_id_zero_when_shawn_mendes_was_sent() {
+        // given
         ArtistRequestDto shawnMendes = ArtistRequestDto.builder()
                 .name("shawn mendes")
                 .build();
+        // when
         ArtistDto response = songifyCrudFacade.addArtist(shawnMendes);
-
+        // then
         assertThat(response.id()).isEqualTo(0L);
         assertThat(response.name()).isEqualTo("shawn mendes");
     }
